fc9630217c72ed6571a7cdd05a8774a37e49c2c2
ljpson <ljpson@naver.com>
2023-04-07 17:26:16 +0900

    DarkMode추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 // import HeaderSon from './son_components/Header/Header';
 // import TodoListSon from './son_components/TodoList/TodoList';
 
@@ -9,12 +10,12 @@ const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0])
   return (
-    <>
+    <DarkModeProvider>
         <Header filters={filters} filter={filter} onFilterChange={setFilter}/>
         <TodoList filter={filter} />
         {/* <HeaderSon filters={filters} filter={filter} onFilterChange={setFilter} />
         <TodoListSon filter={filter} /> */}
-    </>
+    </DarkModeProvider>
   )
 }
 
